bf35569344b9e217dbd186a058ce9e07b5ab14c0
nosmall <elvisotieno24@gmail.com>
2025-05-09 15:05:24 +0300

    adds content

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        if not isinstance(page_count, int):  # Ensure page_count is an integer
+            print("page_count must be an integer")
+            self._page_count = None
+        else:
+            self._page_count = page_count
+
+        self.title = title  # Store the title
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        if not isinstance(size, int):  # Ensure size is an integer
+            print("size must be an integer")
+            self._size = None
+        else:
+            self._size = size
+
+        self.brand = brand  # Store the brand
+        self.condition = "Used"  # Default condition
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set condition to "New" after repair
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
     pass
